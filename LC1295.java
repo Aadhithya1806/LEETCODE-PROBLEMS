e1d75e10dad6752615073f0f9c16fd845ab8a07c
unknowner1818 <aadhithya1837@gmail.com>
2023-11-16 03:29:44 +0530

    Added solution for problem 1295.

---
+++ LC1295.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count;
+        int evenDigits = 0;
+        for (int number : nums) {
+            count = 0;
+            while (number != 0) {
+                number /= 10;
+                count++;
+            }
+            if (count % 2 == 0) {
+                evenDigits++;
+            }
+        }
+        return evenDigits;
+    }
+}
