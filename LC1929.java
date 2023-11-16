565bbe754a86744cc3208932d24ed72bf3c1e815
unknowner1818 <aadhithya1837@gmail.com>
2023-11-16 21:55:47 +0530

    Added solution for problem 1929.

--- LC1929.java
+++ LC1929.java
@@ -1,32 +1,13 @@
-import java.lang.reflect.Array;
+// Solution for problem 1929
 
-class LC1929 {
-    public static void main(String[] args) {
-        int[] arr = { 1, 2, 1 };
-        System.out.println("sadadad");
-        System.out.println(getConcatenation(arr));
-
-    }
-
-    public static int[] getConcatenation(int[] nums) {
-        int newLength = 2 * nums.length;
-        int ans[] = new int[newLength];
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[] = new int[2 * nums.length];
         int index = 0;
-        int i = 0;
         while (index < (2 * nums.length)) {
-
-            if (index == nums.length)
-                i = 0;
-
             ans[index] = nums[index % nums.length];
-            System.out.println("new index " + index);
-            System.out.println("old index " + i);
-            i++;
             index++;
         }
-        System.out.println(index);
-        System.out.println(i);
-
         return ans;
 
     }
