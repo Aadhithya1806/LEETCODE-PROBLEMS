b4005578dc65c87c74581115d61836a5456b5606
unknowner1818 <aadhithya1837@gmail.com>
2023-11-17 23:39:40 +0530

    Added Solution for problem 1512.

--- LC1365.java
+++ LC1365.java
@@ -1,17 +1,18 @@
+// Solution for 1365.
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int ans[]= new int[nums.length];
+        int ans[] = new int[nums.length];
         int count;
-        for(int i = 0 ; i < nums.length;i++){
-            count =0;
-         for(int j = 0 ; j < nums.length;j++){
-            if (j!=i && nums[j]<nums[i]){
-                count++;
+        for (int i = 0; i < nums.length; i++) {
+            count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i && nums[j] < nums[i]) {
+                    count++;
+
+                }
+                ans[i] = count;
 
             }
-            ans[i]=count;
-            
-        }   
         }
         return ans;
     }
