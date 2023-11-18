1eb2eef9e237d9cdcaa8b2ade27b0b9bfa00d76d
unknowner1818 <aadhithya1837@gmail.com>
2023-11-18 22:36:44 +0530

    Added Solution for problem 448.

---
+++ LC448.java
@@ -0,0 +1,28 @@
+// Solution for problem 468.
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else
+                i++;
+
+        }
+        List<Integer> arrayList = new ArrayList<>();
+
+        for (int j = 0; j < nums.length; j++) {
+            if (j + 1 != nums[j])
+                arrayList.add(j + 1);
+        }
+        return arrayList;
+    }
+
+    static void swap(int arr[], int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
