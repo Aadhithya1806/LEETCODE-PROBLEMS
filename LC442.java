8726a1f0edc34b6524c6f46bb38976bbec889d09
unknowner1818 <aadhithya1837@gmail.com>
2023-11-19 11:13:52 +0530

    Added Solution for problem 442.

---
+++ LC442.java
@@ -0,0 +1,28 @@
+// Solution for Problem 442.
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        int i = 0;
+        List<Integer> duplicates = new ArrayList<>();
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                duplicates.add(nums[j]);
+            }
+        }
+        return duplicates;
+    }
+
+    static void swap(int arr[], int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
