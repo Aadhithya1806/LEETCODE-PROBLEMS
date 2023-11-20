acd4002b00a0dda71bb94af89bdbfbaa4af6802c
unknowner1818 <aadhithya1837@gmail.com>
2023-11-20 21:40:53 +0530

    Added Solution for problem 1431.

---
+++ LC1431.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int highestCandy = max(candies);
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            int afterExtra = candies[i] + extraCandies;
+            if (afterExtra >= highestCandy) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        return result;
+    }
+
+    static int max(int arr[]) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
