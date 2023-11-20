14da5ee4b48e1dd84905431908e7374d316b3cab
unknowner1818 <aadhithya1837@gmail.com>
2023-11-20 21:41:12 +0530

    Added Solution for problem 1431.

--- LC1431.java
+++ LC1431.java
@@ -1,3 +1,8 @@
+// Solution for problem 1431.
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int highestCandy = max(candies);
